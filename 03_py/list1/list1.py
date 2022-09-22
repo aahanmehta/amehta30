942c85c342dfca8cbe707588e1318aa64d747a41
Aahan Mehta <aahanmehta@Aahans-MBP.lan>
2022-09-21 22:01:29 -0400

    updated comments

--- 03_py/list1/list1.py
+++ 03_py/list1/list1.py
@@ -1,43 +1,109 @@
+# Since we need to check the last and first elements of the array, take the 0th index and the last index of the array to check if they are equal to 6.
 def first_last6(nums):
   return nums[0] == 6 or nums[len(nums) -1] == 6
 
+first_last6([1, 2, 6]) #→ True
+first_last6([1, 2, 3, 8]) #→ False
+first_last6([6, 6, 6]) #→ True
+
+# First check to see if the array has at least 1 element, then check to see if the first and last elements are equal.
 def same_first_last(nums):
   return len(nums) > 0 and nums[0] == nums[-1]
 
+same_first_last([1, 2, 3]) #→ False
+same_first_last([1, 2, 3, 1]) #→ True
+same_first_last([]) #→ False
+
+# Return an array with 3, 1, and 4 as its elements.
 def make_pi():
   return [3, 1 , 4]
 
+make_pi()
+
+# params: a, b - are both int arrays with length of 1 or greater.
+# First check to see if the arrays have the same elements at index 0, then check their last elements.
 def common_end(a, b):
   return (a[0] == b[0]) or a[-1] == b[-1]
 
+common_end([1, 2, 3], [7, 3]) #→ True
+common_end([1, 2, 3], [7, 3, 2]) #→ False
+common_end([1, 2, 3], [1, 3]) #→ True
+
+# params: nums is an array with length 3.
+# Return the sum of all the three elements.
 def sum3(nums):
   return nums[0] + nums[1] + nums[2]
 
+sum3([1, 2, 3]) #→ 6
+sum3([5, 11, 2]) #→ 18
+sum3([7, 0, 0]) #→ 7
+
+# params: nums- an array of ints has length 3
+# return an array with the last 2 elements of nums in the first 2 indicies, then add on the first element of nums in the 3rd index.
 def rotate_left3(nums):
   return nums[1:] + nums[:1]
 
+rotate_left3([1, 2, 3]) #→ [2, 3, 1]
+rotate_left3([5, 11, 9]) #→ [11, 9, 5]
+rotate_left3([0, 0, 1]) #→ [0, 1, 0]
+
+# params: nums- array of ints with length 3
+# return nums with a step of -1, which reverses the array.
 def reverse3(nums):
   return nums[::-1]
 
+reverse3([1, 2, 3]) #→ [3, 2, 1]
+reverse3([5, 11, 9]) #→ [9, 11, 5]
+reverse3([2, 1, 2]) #→ [2, 1, 2]
+
+# params: nums- array of ints with length 3
+# Checks to see if the first element is larger than the last, if it is, then set all elements equal to the first element, else set all elements equal to the last.
 def max_end3(nums):
   if nums[0] > nums[-1]:
     nums[0], nums[1], nums[2] = nums[0], nums[0], nums[0]
   else:
     nums[0], nums[1], nums[2] = nums[2], nums[2], nums[2]
-
   return nums
 
+max_end3([1, 2, 3]) #→ [3, 3, 3]
+max_end3([11, 5, 9]) #→ [11, 11, 11]
+max_end3([2, 11, 3]) #→ [3, 3, 3]
+
+# params: nums is an array of ints
+# Add up the first 2 elements of the array. nums[:2] returns an array of the first 2 elements, or just the array itself if it has less than length 2.
 def sum2(nums):
   x = 0
   for i in nums[:2]:
     x += i
   return x
 
+sum2([1]) #→ 1
+sum2([1, 2]) #→ 3
+sum2([1, 1, 1, 1]) #→ 2
+
+# params: a, b - are both int arrays of length 3.
+# return an array containing the middle elements in both arrays
 def middle_way(a, b):
   return [a[1], b[1]]
 
+middle_way([1, 2, 3], [4, 5, 6]) #→ [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) #→ [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) #→ [2, 4]
+
+# params: nums is an array of ints
+# return an array with the first and last elements in the array, nums[-1] returns the last element in nums.
 def make_ends(nums):
   return [nums[0], nums[-1]]
 
+make_ends([501, 20, 5]) #→ [501, 5]
+make_ends([1, 2, 3, 1]) #→ [1, 1]
+make_ends([-7, 7]) #→ [-7, 7]
+
+# params: nums - array of length 2
+# 2 in nums checks if nums contains 2. return if nums contains 2 or 3.
 def has23(nums):
   return 2 in nums or 3 in nums
+
+has23([2, 1]) #→ True
+has23([2, 3]) #→ True
+has23([0, 0]) #→ False
--- 03_py/list2/list2.py
+++ 03_py/list2/list2.py
@@ -1,3 +1,5 @@
+# params: nums - an array of ints.
+# use a for loop to check every element of the array for even-ness. increment count by 1 if it is, then return count at the end.
 def count_evens(nums):
   count = 0
   for i in nums:
@@ -5,9 +7,23 @@ def count_evens(nums):
       count += 1
   return count
 
+count_evens([]) #→ 0
+count_evens([2]) #→ 1
+count_evens([2, 1, 2, 2, 40]) #→ 4
+
+# params: nums - array of length 1 or more.
+# max(nums) returns the largest element in nums, min(nums) returns the smallest.
+# return their difference.
 def big_diff(nums):
   return max(nums) - min(nums)
 
+big_diff([2, 3]) #→ 1
+big_diff([2]) #→ 0
+big_diff([100, -100]) #→ 200
+
+# params: nums is an array of len 3 or more.
+# add up all the elements of the array with a for loop, the subtract the largest and smallest values from the sum.
+# finally divide by the total number of elements minus 2 because of the min and max values being excluded.
 def centered_average(nums):
   x, y, z = max(nums), min(nums), 0
   for i in nums:
@@ -16,16 +32,32 @@ def centered_average(nums):
   z = z - x
   return int(z/(len(nums)-2))
 
+centered_average([1, 2, 3, 4, 100]) #→ 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) #→ 5
+centered_average([-10, -4, -2, -4, -2, 0]) #→ -3
+
+# params: nums is an an array of ints
+# use a loop to iterate through nums, checking to see if the current element is equal to 13, if it is, then skip over it and the element after it.
+# if not, then add the element to the sum.
 def sum13(nums):
-  ct, i = 0, 0
+  sum, i = 0, 0
   while i < len(nums):
     if nums[i] == 13:
       i+=2
     else:
-      ct+=nums[i]
+      sum+=nums[i]
       i+=1
   return ct
 
+sum13([13, 1, 2, 13, 2, 1, 13]) #→ 3
+sum13([]) #→ 0
+sum13([1, 2, 2, 1, 13]) #→ 6
+
+# params: nums is an array of ints
+# iterate through the array, while using a boolean to detect if a 6 has already been encountered, and no 7 has been seen yet.
+# if a 6 has been encountered, set the boolean to true, telling the function to not add any other elements until an encounter with a 7 sets the boolean to false.
+# the if-statements for 6 and 7 have to be placed around the sum-adder if-statement so that any 6s or 7s are not added.
+# * the 7 is still added if it doesnt change the boolean.
 def sum67(nums):
   inBetween = False
   sum = 0
@@ -38,8 +70,20 @@ def sum67(nums):
       inBetween = False
   return sum
 
+sum67([]) #→ 0
+sum67([2, 7, 6, 2, 6, 2, 7]) #→ 9
+sum67([1, 2, 2, 6, 99, 99, 7]) #→ 5
+
+# params: nums is an array of ints
+# iterate through the array, excluding the last element to prevent any index out of bounds errors —
+# this is becuase we checked to see if both the current element and the element right after it were equal to 2.
+# if this never happens throughout the array, return false. return true if both elements are 2.
 def has22(nums):
   for i in range(len(nums) -1):
     if nums[i] == 2 and nums[i+1] == 2:
       return True
   return False
+
+has22([2, 2, 1, 2]) #→ True
+has22([2]) #→ False
+has22([5, 2, 5, 2]) #→ False

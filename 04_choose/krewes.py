51ae1e3f172a3aae53995086c561c8cf42192a2e
Aahan Mehta <aahanmehta@Aahans-MBP.lan>
2022-09-22 21:02:33 -0400

    trial solution of rand dict roster

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,8 +1,13 @@
+import random
 '''
 Team Smartans: Aahan M, Wilson M, Joshua L
 DISCO:
 QCC:
 OPS SUMMARY:
 '''
-krewes = {}
+krewes = {1: ["a","b"], 2: ["c","d"], 3: ["e", "f"]}
 
+x=[]
+x= list(krewes.keys())
+#print(random.choice(x))
+print(random.choice(krewes[x[random.choice(range(len(x)))]]))

9f82a58fbd06be6e50f7508b96694665508b6b96
Aahan Mehta <aahanmehta@Aahans-MacBook-Pro.local>
2022-10-01 22:35:33 -0400

    cleaner file reading

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -23,33 +23,29 @@ import random
 def make_dict(filename):
     dict = {} #makes dict to return
     file = open(filename)
-    file_str = file.read()[21:]
+    file_str = file.read()
     file.close()
-    #file_str = file_str.replace("\n","") #deals with newlines from texeditor
     #print(file_str)
     devo_info_str = file_str.split("\n") #gives unformatted str of each devo
-    #print(devo_info_str)
-    devo_info_str = devo_info_str[:-1]
+    # print(devo_info_str)
+    devo_info_str = devo_info_str[1:-1]
     for devo in devo_info_str:
         if (devo[0:1] == '"'):
             infos = devo.rsplit(",",1)
-            #for n in devo:
         else:
             infos = devo.split(",") #devos info separated
-        print(infos)
+        # print(infos)
         key = infos[0]
         if key not in dict:
-            dict[key] = [] #make new key if pd isn't alr in dictionary
-        dict[key] = float(infos[1]) #appends tuple of dvo+ducky
+            dict[key] = None #make new key if pd isn't alr in dictionary
+        dict[key] = float(infos[1]) #pairs percent with job (key)
+    print(list(dict.keys()))
     return dict
-
-
-
 
 def get_rand_weighted_krewe(nums):
     if(len(nums) == 0):
         return ["empty dict"]
-    key = random.choices(list(nums.keys())[1:-1], weights=list(nums.values())[1:-1], k=1)
+    key = random.choices(list(nums.keys()), weights=list(nums.values()), k=1)
     return key
 
 

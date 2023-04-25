bafd2213a65438d0b7c07238466008249aa0a098
Aahan Mehta <aahanmehta@Aahans-MacBook-Pro.local>
2023-04-25 13:30:02 -0400

    updates

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -11,18 +11,52 @@ var mode = "rect";
 var toggleMode = (e) => {
     console.log("toggling...");
     if (mode === "rect") {
-        mode = "circ"
+        mode = "circ";
     }
     else {
-        mode = "rect"
+        mode = "rect";
     }
 }
 
 var drawRect = function(e) {
-    var mouseX = e.clientX //gets X-coor of mouse when event is fired
-    var mouseY = e.clientY //gets Y-coor of mouse when event is fired
-    console.log("mouseclick registered at ", mouseX, mouseY)
-    ctx.fillRect(mouseX, mouseY, 10, 10)
+    var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
+    var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(mouseX, mouseY, 100, 200)
 }
 
-c.addEventListener("click", drawRect) //passes the mouse event as parameter for the function
+var drawCircle = (e) => {
+    var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
+    var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath();
+    ctx.fillStyle = "#FF0000";
+    ctx.arc(mouseX, mouseY, 50, 0,2 *Math.PI);
+    ctx.fillstyle = "red";
+    ctx.fill();
+}
+
+var draw = (e) => {
+    if (mode == "rect"){
+        drawRect(e);
+    } else{
+        drawCircle(e);
+    }
+}
+
+var wipeCanvas = () => {
+    ctx.clearRect(0,0, 600, 600);
+}
+
+c.addEventListener("click", draw) //passes the mouse event as parameter for the function
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener('click', () =>{
+    toggleMode();
+
+});
+
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener('click', () =>{
+    wipeCanvas();
+});
